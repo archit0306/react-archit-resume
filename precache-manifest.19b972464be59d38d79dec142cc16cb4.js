b61608aff4219855c7866142a261aa064fbae62d
archit0306 <archit0306@gmail.com>
2020-08-04 21:55:11 +0530

    Updates

---
+++ precache-manifest.19b972464be59d38d79dec142cc16cb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c8429b5002db1dbd80e405d0f9786ab",
+    "url": "/react-archit-resume/index.html"
+  },
+  {
+    "revision": "991f6df572da25aa6a4a",
+    "url": "/react-archit-resume/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "3aac042792b1606680bd",
+    "url": "/react-archit-resume/static/js/2.33ae6a19.chunk.js"
+  },
+  {
+    "revision": "eff758343d82f754e4881ed2e608a50b",
+    "url": "/react-archit-resume/static/js/2.33ae6a19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "991f6df572da25aa6a4a",
+    "url": "/react-archit-resume/static/js/main.01baf1ff.chunk.js"
+  },
+  {
+    "revision": "48cbc1870fb8c5f41f6d",
+    "url": "/react-archit-resume/static/js/runtime-main.f58b064c.js"
+  }
+]);
